9df6b1aec1716ecc03d21096982b8b475345dcf2
AshleyDcksn <dicksona@mail.usf.edu>
2015-11-10 10:45:23 -0500

    Adding data to dictionary

--- CYK_Membership_Program/Form1.Designer.cs
+++ CYK_Membership_Program/Form1.Designer.cs
@@ -29,23 +29,24 @@
         private void InitializeComponent()
         {
             this.title_lbl = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.textBox8 = new System.Windows.Forms.TextBox();
-            this.textBox9 = new System.Windows.Forms.TextBox();
-            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.tbVar1 = new System.Windows.Forms.TextBox();
+            this.tbVar5 = new System.Windows.Forms.TextBox();
+            this.tbVar4 = new System.Windows.Forms.TextBox();
+            this.tbVar3 = new System.Windows.Forms.TextBox();
+            this.tbVar2 = new System.Windows.Forms.TextBox();
+            this.tbData1 = new System.Windows.Forms.TextBox();
+            this.tbData3 = new System.Windows.Forms.TextBox();
+            this.tbData4 = new System.Windows.Forms.TextBox();
+            this.tbData5 = new System.Windows.Forms.TextBox();
+            this.tbData2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.tbWord = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // title_lbl
@@ -58,85 +59,85 @@
             this.title_lbl.TabIndex = 0;
             this.title_lbl.Text = "Please enter grammar in Chromsky Normal Form:";
             // 
-            // textBox1
+            // tbVar1
             // 
-            this.textBox1.Location = new System.Drawing.Point(40, 51);
-            this.textBox1.MaxLength = 1;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(29, 20);
-            this.textBox1.TabIndex = 1;
+            this.tbVar1.Location = new System.Drawing.Point(40, 51);
+            this.tbVar1.MaxLength = 1;
+            this.tbVar1.Name = "tbVar1";
+            this.tbVar1.Size = new System.Drawing.Size(29, 20);
+            this.tbVar1.TabIndex = 1;
             // 
-            // textBox2
+            // tbVar5
             // 
-            this.textBox2.Location = new System.Drawing.Point(40, 203);
-            this.textBox2.MaxLength = 1;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(29, 20);
-            this.textBox2.TabIndex = 9;
+            this.tbVar5.Location = new System.Drawing.Point(40, 203);
+            this.tbVar5.MaxLength = 1;
+            this.tbVar5.Name = "tbVar5";
+            this.tbVar5.Size = new System.Drawing.Size(29, 20);
+            this.tbVar5.TabIndex = 9;
             // 
-            // textBox3
+            // tbVar4
             // 
-            this.textBox3.Location = new System.Drawing.Point(40, 166);
-            this.textBox3.MaxLength = 1;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(29, 20);
-            this.textBox3.TabIndex = 7;
+            this.tbVar4.Location = new System.Drawing.Point(40, 166);
+            this.tbVar4.MaxLength = 1;
+            this.tbVar4.Name = "tbVar4";
+            this.tbVar4.Size = new System.Drawing.Size(29, 20);
+            this.tbVar4.TabIndex = 7;
             // 
-            // textBox4
+            // tbVar3
             // 
-            this.textBox4.Location = new System.Drawing.Point(40, 125);
-            this.textBox4.MaxLength = 1;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(29, 20);
-            this.textBox4.TabIndex = 5;
+            this.tbVar3.Location = new System.Drawing.Point(40, 125);
+            this.tbVar3.MaxLength = 1;
+            this.tbVar3.Name = "tbVar3";
+            this.tbVar3.Size = new System.Drawing.Size(29, 20);
+            this.tbVar3.TabIndex = 5;
             // 
-            // textBox5
+            // tbVar2
             // 
-            this.textBox5.Location = new System.Drawing.Point(40, 89);
-            this.textBox5.MaxLength = 1;
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(29, 20);
-            this.textBox5.TabIndex = 3;
+            this.tbVar2.Location = new System.Drawing.Point(40, 89);
+            this.tbVar2.MaxLength = 1;
+            this.tbVar2.Name = "tbVar2";
+            this.tbVar2.Size = new System.Drawing.Size(29, 20);
+            this.tbVar2.TabIndex = 3;
             // 
-            // textBox6
+            // tbData1
             // 
-            this.textBox6.Location = new System.Drawing.Point(142, 51);
-            this.textBox6.MaxLength = 2;
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 20);
-            this.textBox6.TabIndex = 2;
+            this.tbData1.Location = new System.Drawing.Point(142, 51);
+            this.tbData1.MaxLength = 2;
+            this.tbData1.Name = "tbData1";
+            this.tbData1.Size = new System.Drawing.Size(100, 20);
+            this.tbData1.TabIndex = 2;
             // 
-            // textBox7
+            // tbData3
             // 
-            this.textBox7.Location = new System.Drawing.Point(142, 125);
-            this.textBox7.MaxLength = 2;
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(100, 20);
-            this.textBox7.TabIndex = 6;
+            this.tbData3.Location = new System.Drawing.Point(142, 125);
+            this.tbData3.MaxLength = 2;
+            this.tbData3.Name = "tbData3";
+            this.tbData3.Size = new System.Drawing.Size(100, 20);
+            this.tbData3.TabIndex = 6;
             // 
-            // textBox8
+            // tbData4
             // 
-            this.textBox8.Location = new System.Drawing.Point(142, 166);
-            this.textBox8.MaxLength = 2;
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(100, 20);
-            this.textBox8.TabIndex = 8;
+            this.tbData4.Location = new System.Drawing.Point(142, 166);
+            this.tbData4.MaxLength = 2;
+            this.tbData4.Name = "tbData4";
+            this.tbData4.Size = new System.Drawing.Size(100, 20);
+            this.tbData4.TabIndex = 8;
             // 
-            // textBox9
+            // tbData5
             // 
-            this.textBox9.Location = new System.Drawing.Point(142, 203);
-            this.textBox9.MaxLength = 2;
-            this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(100, 20);
-            this.textBox9.TabIndex = 10;
+            this.tbData5.Location = new System.Drawing.Point(142, 203);
+            this.tbData5.MaxLength = 2;
+            this.tbData5.Name = "tbData5";
+            this.tbData5.Size = new System.Drawing.Size(100, 20);
+            this.tbData5.TabIndex = 10;
             // 
-            // textBox10
+            // tbData2
             // 
-            this.textBox10.Location = new System.Drawing.Point(142, 89);
-            this.textBox10.MaxLength = 2;
-            this.textBox10.Name = "textBox10";
-            this.textBox10.Size = new System.Drawing.Size(100, 20);
-            this.textBox10.TabIndex = 4;
+            this.tbData2.Location = new System.Drawing.Point(142, 89);
+            this.tbData2.MaxLength = 2;
+            this.tbData2.Name = "tbData2";
+            this.tbData2.Size = new System.Drawing.Size(100, 20);
+            this.tbData2.TabIndex = 4;
             // 
             // label1
             // 
@@ -198,33 +199,45 @@
             this.label6.TabIndex = 16;
             this.label6.Text = "Please enter a word you would like to test for membership:";
             // 
-            // textBox11
+            // tbWord
             // 
-            this.textBox11.Location = new System.Drawing.Point(40, 280);
-            this.textBox11.Name = "textBox11";
-            this.textBox11.Size = new System.Drawing.Size(202, 20);
-            this.textBox11.TabIndex = 17;
+            this.tbWord.Location = new System.Drawing.Point(40, 280);
+            this.tbWord.Name = "tbWord";
+            this.tbWord.Size = new System.Drawing.Size(202, 20);
+            this.tbWord.TabIndex = 17;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGo.Location = new System.Drawing.Point(18, 326);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 18;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(737, 333);
-            this.Controls.Add(this.textBox11);
+            this.ClientSize = new System.Drawing.Size(423, 361);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.tbWord);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox10);
-            this.Controls.Add(this.textBox9);
-            this.Controls.Add(this.textBox8);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbData2);
+            this.Controls.Add(this.tbData5);
+            this.Controls.Add(this.tbData4);
+            this.Controls.Add(this.tbData3);
+            this.Controls.Add(this.tbData1);
+            this.Controls.Add(this.tbVar2);
+            this.Controls.Add(this.tbVar3);
+            this.Controls.Add(this.tbVar4);
+            this.Controls.Add(this.tbVar5);
+            this.Controls.Add(this.tbVar1);
             this.Controls.Add(this.title_lbl);
             this.Name = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load_1);
@@ -236,23 +249,24 @@
         #endregion
         
         private System.Windows.Forms.Label title_lbl;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.TextBox textBox8;
-        private System.Windows.Forms.TextBox textBox9;
-        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox tbVar1;
+        private System.Windows.Forms.TextBox tbVar5;
+        private System.Windows.Forms.TextBox tbVar4;
+        private System.Windows.Forms.TextBox tbVar3;
+        private System.Windows.Forms.TextBox tbVar2;
+        private System.Windows.Forms.TextBox tbData1;
+        private System.Windows.Forms.TextBox tbData3;
+        private System.Windows.Forms.TextBox tbData4;
+        private System.Windows.Forms.TextBox tbData5;
+        private System.Windows.Forms.TextBox tbData2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox tbWord;
+        private System.Windows.Forms.Button btnGo;
 
         
     }
--- CYK_Membership_Program/Form1.cs
+++ CYK_Membership_Program/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 
 namespace CYK_Membership_Program
 {
+    
     public partial class Form1 : Form
     {
         public Form1()
@@ -28,6 +29,39 @@ namespace CYK_Membership_Program
         private void Form1_Load_1(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Add input data to dictionary
+            Dictionary<string, char[]> dict = new Dictionary<string, char[]>();
+            char[] Data1 = tbData1.Text.ToCharArray();
+            dict.Add(tbVar1.Text, Data1);
+            char[] Data2, Data3, Data4, Data5;
+            if (tbData2.Text != "")
+            {
+                Data2 = tbData2.Text.ToCharArray();
+                dict.Add(tbVar2.Text, Data2);
+            }
+            if (tbData3.Text != "")
+            {
+                Data3 = tbData3.Text.ToCharArray();
+                dict.Add(tbVar3.Text, Data3);
+            }
+            if (tbData4.Text != "")
+            {
+                Data4 = tbData4.Text.ToCharArray();
+                dict.Add(tbVar4.Text, Data4);
+            }
+            if (tbData5.Text != "")
+            {
+                Data5 = tbData5.Text.ToCharArray();
+                dict.Add(tbVar5.Text, Data5);
+            }
+            
+            
+                
+            
         }
 
     }
