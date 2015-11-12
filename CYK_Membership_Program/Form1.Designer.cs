3e82fe7ac819c6d479952976645d8b075dcdd4b0
AshleyDcksn <dicksona@mail.usf.edu>
2015-11-11 21:52:52 -0500

    completed

--- CYK_Membership_Program/Form1.Designer.cs
+++ CYK_Membership_Program/Form1.Designer.cs
@@ -64,30 +64,32 @@
             this.X25 = new System.Windows.Forms.Label();
             this.label22 = new System.Windows.Forms.Label();
             this.lblResults = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // title_lbl
             // 
             this.title_lbl.AutoSize = true;
             this.title_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.title_lbl.Location = new System.Drawing.Point(12, 21);
+            this.title_lbl.Location = new System.Drawing.Point(12, 25);
             this.title_lbl.Name = "title_lbl";
             this.title_lbl.Size = new System.Drawing.Size(318, 17);
             this.title_lbl.TabIndex = 0;
-            this.title_lbl.Text = "Please enter grammar in Chromsky Normal Form:";
+            this.title_lbl.Text = "Please enter grammar in Chromsky Normal Form ";
             // 
             // tbVar1
             // 
-            this.tbVar1.Location = new System.Drawing.Point(40, 51);
+            this.tbVar1.Location = new System.Drawing.Point(40, 125);
             this.tbVar1.MaxLength = 1;
             this.tbVar1.Name = "tbVar1";
             this.tbVar1.Size = new System.Drawing.Size(29, 20);
             this.tbVar1.TabIndex = 1;
-            this.tbVar1.Text = "S";
             // 
             // tbVar5
             // 
-            this.tbVar5.Location = new System.Drawing.Point(40, 203);
+            this.tbVar5.Location = new System.Drawing.Point(40, 268);
             this.tbVar5.MaxLength = 1;
             this.tbVar5.Name = "tbVar5";
             this.tbVar5.Size = new System.Drawing.Size(29, 20);
@@ -95,61 +97,55 @@
             // 
             // tbVar4
             // 
-            this.tbVar4.Location = new System.Drawing.Point(40, 166);
+            this.tbVar4.Location = new System.Drawing.Point(40, 233);
             this.tbVar4.MaxLength = 1;
             this.tbVar4.Name = "tbVar4";
             this.tbVar4.Size = new System.Drawing.Size(29, 20);
             this.tbVar4.TabIndex = 7;
-            this.tbVar4.Text = "C";
             // 
             // tbVar3
             // 
-            this.tbVar3.Location = new System.Drawing.Point(40, 125);
+            this.tbVar3.Location = new System.Drawing.Point(40, 200);
             this.tbVar3.MaxLength = 1;
             this.tbVar3.Name = "tbVar3";
             this.tbVar3.Size = new System.Drawing.Size(29, 20);
             this.tbVar3.TabIndex = 5;
-            this.tbVar3.Text = "B";
             // 
             // tbVar2
             // 
-            this.tbVar2.Location = new System.Drawing.Point(40, 89);
+            this.tbVar2.Location = new System.Drawing.Point(40, 166);
             this.tbVar2.MaxLength = 1;
             this.tbVar2.Name = "tbVar2";
             this.tbVar2.Size = new System.Drawing.Size(29, 20);
             this.tbVar2.TabIndex = 3;
-            this.tbVar2.Text = "A";
             // 
             // tbData1
             // 
-            this.tbData1.Location = new System.Drawing.Point(142, 51);
+            this.tbData1.Location = new System.Drawing.Point(142, 125);
             this.tbData1.MaxLength = 10;
             this.tbData1.Name = "tbData1";
             this.tbData1.Size = new System.Drawing.Size(100, 20);
             this.tbData1.TabIndex = 2;
-            this.tbData1.Text = "AB | BC";
             // 
             // tbData3
             // 
-            this.tbData3.Location = new System.Drawing.Point(142, 125);
+            this.tbData3.Location = new System.Drawing.Point(142, 200);
             this.tbData3.MaxLength = 10;
             this.tbData3.Name = "tbData3";
             this.tbData3.Size = new System.Drawing.Size(100, 20);
             this.tbData3.TabIndex = 6;
-            this.tbData3.Text = "CC | b";
             // 
             // tbData4
             // 
-            this.tbData4.Location = new System.Drawing.Point(142, 166);
+            this.tbData4.Location = new System.Drawing.Point(142, 233);
             this.tbData4.MaxLength = 10;
             this.tbData4.Name = "tbData4";
             this.tbData4.Size = new System.Drawing.Size(100, 20);
             this.tbData4.TabIndex = 8;
-            this.tbData4.Text = "AB | a";
             // 
             // tbData5
             // 
-            this.tbData5.Location = new System.Drawing.Point(142, 203);
+            this.tbData5.Location = new System.Drawing.Point(142, 268);
             this.tbData5.MaxLength = 10;
             this.tbData5.Name = "tbData5";
             this.tbData5.Size = new System.Drawing.Size(100, 20);
@@ -157,18 +153,17 @@
             // 
             // tbData2
             // 
-            this.tbData2.Location = new System.Drawing.Point(142, 89);
+            this.tbData2.Location = new System.Drawing.Point(142, 166);
             this.tbData2.MaxLength = 10;
             this.tbData2.Name = "tbData2";
             this.tbData2.Size = new System.Drawing.Size(100, 20);
             this.tbData2.TabIndex = 4;
-            this.tbData2.Text = "BA | a";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(85, 42);
+            this.label1.Location = new System.Drawing.Point(85, 116);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(37, 29);
             this.label1.TabIndex = 11;
@@ -178,7 +173,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(85, 195);
+            this.label2.Location = new System.Drawing.Point(85, 259);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(37, 29);
             this.label2.TabIndex = 12;
@@ -188,7 +183,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(85, 157);
+            this.label3.Location = new System.Drawing.Point(85, 224);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(37, 29);
             this.label3.TabIndex = 13;
@@ -198,7 +193,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(85, 116);
+            this.label4.Location = new System.Drawing.Point(85, 191);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(37, 29);
             this.label4.TabIndex = 14;
@@ -208,7 +203,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(85, 87);
+            this.label5.Location = new System.Drawing.Point(85, 157);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(37, 29);
             this.label5.TabIndex = 15;
@@ -218,25 +213,24 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(15, 259);
+            this.label6.Location = new System.Drawing.Point(12, 309);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(375, 17);
+            this.label6.Size = new System.Drawing.Size(272, 17);
             this.label6.TabIndex = 16;
-            this.label6.Text = "Please enter a word you would like to test for membership:";
+            this.label6.Text = " Please enter of word of length 5 that you ";
             // 
             // tbWord
             // 
-            this.tbWord.Location = new System.Drawing.Point(40, 280);
+            this.tbWord.Location = new System.Drawing.Point(40, 358);
             this.tbWord.MaxLength = 5;
             this.tbWord.Name = "tbWord";
             this.tbWord.Size = new System.Drawing.Size(202, 20);
             this.tbWord.TabIndex = 17;
-            this.tbWord.Text = "baaba";
             // 
             // btnGo
             // 
             this.btnGo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnGo.Location = new System.Drawing.Point(18, 326);
+            this.btnGo.Location = new System.Drawing.Point(15, 384);
             this.btnGo.Name = "btnGo";
             this.btnGo.Size = new System.Drawing.Size(75, 23);
             this.btnGo.TabIndex = 18;
@@ -413,9 +407,44 @@
             this.lblResults.Size = new System.Drawing.Size(0, 17);
             this.lblResults.TabIndex = 35;
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 51);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(176, 17);
+            this.label7.TabIndex = 36;
+            this.label7.Text = "or choose a sample below:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Sample 1",
+            "Sample 2"});
+            this.comboBox1.Location = new System.Drawing.Point(40, 84);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(202, 21);
+            this.comboBox1.TabIndex = 37;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(26, 326);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(174, 17);
+            this.label8.TabIndex = 38;
+            this.label8.Text = "like to test for membership";
+            // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(821, 361);
+            this.ClientSize = new System.Drawing.Size(826, 432);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.lblResults);
             this.Controls.Add(this.label22);
             this.Controls.Add(this.X25);
@@ -497,6 +526,9 @@
         private System.Windows.Forms.Label X25;
         private System.Windows.Forms.Label label22;
         private System.Windows.Forms.Label lblResults;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label8;
 
         
     }
--- CYK_Membership_Program/Form1.cs
+++ CYK_Membership_Program/Form1.cs
@@ -16,6 +16,8 @@ namespace CYK_Membership_Program
         //Declare Global vars
         Dictionary<string, string> dict = new Dictionary<string, string>();
         List<string> rhs = new List<string>(); //Holds right hand side of grammar
+        
+        //Using a list of strings for every table entry
         //For row 1 and 2 comparison
         List<string> var_list1 = new List<string>();
         List<string> var_list2 = new List<string>();
@@ -27,11 +29,19 @@ namespace CYK_Membership_Program
         List<string> var_list7 = new List<string>();
         List<string> var_list8 = new List<string>();
         List<string> var_list9 = new List<string>();
+        List<string> var_list10 = new List<string>();
+        List<string> var_list11 = new List<string>();
+        List<string> var_list12 = new List<string>();
+        List<string> var_list13 = new List<string>();
+        List<string> var_list14 = new List<string>();
+        List<string> var_list15 = new List<string>();
+
         
         public Form1()
         {
             InitializeComponent();
             this.Text = "CYK Membership Program";   
+            
         }
 
         private void Form1_Load(object sender, EventArgs e){         
@@ -99,6 +109,8 @@ namespace CYK_Membership_Program
             }
             fill_in_row_2(sender, word_length);
             fill_in_row_3(sender, word_length);
+            fill_in_row_4(sender, word_length);
+            fill_in_row_5(sender, word_length);
         
         }
         private void init_table(object sender, int word_length)
@@ -167,14 +179,14 @@ namespace CYK_Membership_Program
                 X23.Text = "X23 = "; //X23 = X22; X33
                 X34.Text = "X34 = "; //X34 = X33; X44
                 X45.Text = "X45 = "; //X44 = X44; X55
-                var_list1 = perform_concat(var_list1, var_list2);
-                var_list2 = perform_concat(var_list2, var_list3);
-                var_list3 = perform_concat(var_list3, var_list4);
-                var_list4 = perform_concat(var_list4, var_list5);
+                var_list15 = perform_concat(var_list1, var_list2);
+                var_list14 = perform_concat(var_list2, var_list3);
+                var_list13 = perform_concat(var_list3, var_list4);
+                var_list12 = perform_concat(var_list4, var_list5);
                 foreach (KeyValuePair<string, string> kvp in dict)
                 {
                     
-                    foreach (var item in var_list1)
+                    foreach (var item in var_list15)
                     {
                         if (kvp.Value.Contains(item))
                         {
@@ -183,7 +195,7 @@ namespace CYK_Membership_Program
                         }
                     }
                     
-                    foreach (var item in var_list2)
+                    foreach (var item in var_list14)
                     {
                         if (kvp.Value.Contains(item))
                         {
@@ -192,7 +204,7 @@ namespace CYK_Membership_Program
                         }
                     }
                    
-                    foreach (var item in var_list3)
+                    foreach (var item in var_list13)
                     {
                         if (kvp.Value.Contains(item))
                         {
@@ -201,7 +213,7 @@ namespace CYK_Membership_Program
                         }
                     }
                     
-                    foreach (var item in var_list4)
+                    foreach (var item in var_list12)
                     {
                         if (kvp.Value.Contains(item))
                         {
@@ -214,50 +226,70 @@ namespace CYK_Membership_Program
                 X23.Visible = true;
                 X34.Visible = true;
                 X45.Visible = true;
-                
+                var_list15.Clear();
+                var_list14.Clear();
+                var_list13.Clear();
+                var_list12.Clear();
             
             }
         }
+        //X15 = x11 x25, x12 x35, x13 X45, X14 x55
         private void fill_in_row_3(object sender, int word_length)
         {
             if (word_length == 5)
             {
                 //Go through the previous row and check for matches
-                X13.Text = "X13 = "; //X13 = X11X23; X12, X33
-                X24.Text = "X24 = ";
-                X35.Text = "X35 = ";
-                var_list6 = perform_concat(var_list6, var_list7);
-                var_list7 = perform_concat(var_list7, var_list8);
-                var_list8 = perform_concat(var_list8, var_list9);
-                //Clear lists 1,2, 3 to use for fill_in_row_4
-                var_list1.Clear();
-                var_list2.Clear();
-                var_list3.Clear();
+                X13.Text = "X13 = "; //X13 = X11 X23; X12 X33
+                X24.Text = "X24 = "; //X24 = X22 X34; X23 X44
+                X35.Text = "X35 = "; //X35 = X33 X45; X34 X55
+                List<string> temp = new List<string>();
+                List<string> temp2 = new List<string>();
+                //X13
+                temp = perform_concat(var_list1, var_list7);
+                temp2 = perform_concat(var_list6, var_list3);
+                var_list15.AddRange(temp);
+                var_list15.AddRange(temp2);
+                temp.Clear();
+                temp2.Clear();
+                //X24
+                temp = perform_concat(var_list2, var_list8);
+                temp2 = perform_concat(var_list7, var_list4);
+                var_list14.AddRange(temp);
+                var_list14.AddRange(temp2);
+                temp.Clear();
+                temp2.Clear();
+                //X35
+                temp = perform_concat(var_list3, var_list9);
+                temp2 = perform_concat(var_list8, var_list5);
+                var_list13.AddRange(temp);
+                var_list13.AddRange(temp2);
+
                 foreach (KeyValuePair<string, string> kvp in dict)
                 {
 
-                    foreach (var item in var_list6)
+                    foreach (var item in var_list15)
                     {
                         if (kvp.Value.Contains(item))
                         {
+                            
                             X13.Text += kvp.Key;
-                            var_list1.Add(kvp.Key);
+                            var_list10.Add(kvp.Key);
                         }
                     }
-                    foreach (var item in var_list7)
+                    foreach (var item in var_list14)
                     {
                         if (kvp.Value.Contains(item))
                         {
                             X24.Text += kvp.Key;
-                            var_list2.Add(kvp.Key);
+                            var_list11.Add(kvp.Key);
                         }
                     }
-                    foreach (var item in var_list8)
+                    foreach (var item in var_list13)
                     {
                         if (kvp.Value.Contains(item))
                         {
                             X35.Text += kvp.Key;
-                            var_list3.Add(kvp.Key);
+                            var_list12.Add(kvp.Key);
                         }
                     }
 
@@ -266,23 +298,153 @@ namespace CYK_Membership_Program
             X13.Visible = true;
             X24.Visible = true;
             X35.Visible = true;
-            
+            var_list15.Clear();
+            var_list14.Clear();
+            var_list13.Clear();
+        }
+        private void fill_in_row_4(object sender, int word_length)
+        {
+            if (word_length == 5)
+            {
+                //Go through the previous rows and check for matches
+                X14.Text = "X14 = "; //X14 = X11 X24 U X12 X34 U X13 X44
+                X25.Text = "X25 = "; //X25 = X22 X35 U X23 X45 U X24 X55
+                List<string> temp = new List<string>();
+                List<string> temp2 = new List<string>();
+                List<string> temp3 = new List<string>();
+                //X14
+                temp = perform_concat(var_list1, var_list11);
+                temp2 = perform_concat(var_list6, var_list8);
+                temp3 = perform_concat(var_list10, var_list4);
+                var_list15.AddRange(temp);
+                var_list15.AddRange(temp2);
+                var_list15.AddRange(temp3);
+                var_list15 = var_list15.Distinct().ToList();
+                temp.Clear();
+                temp2.Clear();
+                temp3.Clear();
+                //X25
+                temp = perform_concat(var_list2, var_list12);
+                temp2 = perform_concat(var_list7, var_list9);
+                temp3 = perform_concat(var_list11, var_list5);
+                temp.AddRange(temp2);
+                temp.AddRange(temp3);
+                temp = temp.Distinct().ToList();
+                temp2.Clear();
+                temp3.Clear();
+                Console.WriteLine("PRINTING 15");
+                foreach (var item in temp)
+                {
+                    Console.WriteLine(item);
+                }
+                foreach (KeyValuePair<string, string> kvp in dict)
+                {
+
+                    foreach (var item in var_list15)
+                    {
+                        if (kvp.Value.Contains(item))
+                        {
+                            X14.Text += kvp.Key;
+                            var_list13.Add(kvp.Key);
+                        }
+                    }
+                    foreach (var item in temp)
+                    {
+                        if (kvp.Value.Contains(item))
+                        {
+                            X25.Text += kvp.Key;
+                            var_list14.Add(kvp.Key);
+                        }
+                    }
+                }
+                //Removing duplicate keys
+                X14.Text = RemoveDuplicates(X14.Text);
+                X25.Text = RemoveDuplicates(X25.Text);
+                
+                X14.Visible = true;
+                X25.Visible = true;
+                var_list15.Clear();
+            }
+        
+        }
+        private void fill_in_row_5(object sender, int word_length)
+        {
+            if (word_length == 5)
+            {
+                //Go through the previous rows and check for matches
+                X15.Text = "X15 = "; //X15 = X11 X25 U X12 X35 U X13 X45 U X14 X55
+                List<string> temp = new List<string>();
+                List<string> temp2 = new List<string>();
+                List<string> temp3 = new List<string>();
+                List<string> temp4 = new List<string>();
+                //X15
+                temp = perform_concat(var_list1, var_list14);
+                temp2 = perform_concat(var_list6, var_list12);
+                temp3 = perform_concat(var_list10, var_list9);
+                temp4 = perform_concat(var_list13, var_list5);
+                temp.AddRange(temp2);
+                temp.AddRange(temp2);
+                temp.AddRange(temp3);
+                temp.AddRange(temp4);
+                foreach (KeyValuePair<string, string> kvp in dict)
+                {
+
+                    foreach (var item in temp)
+                    {
+                        if (kvp.Value.Contains(item))
+                        {
+                            X15.Text += kvp.Key;
+                            var_list15.Add(kvp.Key);
+                        }
+                    }
+                }
+                //Removing duplicate keys
+                X15.Text = RemoveDuplicates(X15.Text);
+                X15.Visible = true;
+                if (var_list15.Count > 0)
+                {
+                    lblResults.Text = "The word is a member of the grammar!\n";
+                }
+                else
+                {
+                    lblResults.Text = "The word is Not a member of the grammar.\n";
+                }
+            }
+        
+        
         }
         private List<string> perform_concat(List<string> list1, List<string> list2)
         {
-            
-            List<string> concat_list = new List<string>();
-            foreach (var item in list1)
+
+            if (list1.Count != 0 && list2.Count != 0)
             {
-                foreach (var item2 in list2)
+                List<string> concat_list = new List<string>();
+                foreach (var item in list1)
                 {
-                    string items = item + item2;
-                    concat_list.Add(items);
+                    foreach (var item2 in list2)
+                    {
+                        string items = item + item2;
+                        concat_list.Add(items);
+                    }
                 }
+                Console.WriteLine("Length of concatted list is:");
+                Console.WriteLine(concat_list.Count);
+                return concat_list;
             }
-            Console.WriteLine("Length of concatted list is:");
-            Console.WriteLine(concat_list.Count);
-            return concat_list;
+            else if (list1.Count == 0)
+            {
+                return list1;
+            }
+            else if (list2.Count == 0)
+            {
+                return list2;
+            }
+            else
+                return list1;
+        }
+        private static string RemoveDuplicates(string input)
+        {
+            return new string(input.ToCharArray().Distinct().ToArray());
         }
         private void clear_all_data_structures()
         { 
@@ -296,9 +458,47 @@ namespace CYK_Membership_Program
             var_list7.Clear();
             var_list8.Clear();
             var_list9.Clear();
+            var_list10.Clear();
+            var_list11.Clear();
+            var_list12.Clear();
+            var_list13.Clear();
+            var_list14.Clear();
+            var_list15.Clear();
             rhs.Clear();
 
         }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                //Load with pre-filled grammar
+                tbVar1.Text = "S";
+                tbData1.Text = "AB | BC";
+                tbVar2.Text = "A";
+                tbData2.Text = "BA | a";
+                tbVar3.Text = "B";
+                tbData3.Text = "CC | b";
+                tbVar4.Text = "C";
+                tbData4.Text = "AB | a";
+                tbWord.Text = "baaba";
+            }
+            if (comboBox1.SelectedIndex == 1)
+            {
+                //Load with pre-filled grammar
+                tbVar1.Text = "S";
+                tbData1.Text = "AB";
+                tbVar2.Text = "A";
+                tbData2.Text = "BC | a";
+                tbVar3.Text = "B";
+                tbData3.Text = "AC | b";
+                tbVar4.Text = "C";
+                tbData4.Text = "a | b";
+                tbWord.Text = "ababa";
+            
+            }
+        }
+
   
 
     }
